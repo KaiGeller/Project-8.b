2b666ec28c044dd45e4f4c44f165f082394e7dd4
KaiGeller <gellerka@oregonstate.edu>
2022-05-17 20:46:00 -0700

    1

--- word_in_both.py
+++ word_in_both.py
@@ -3,6 +3,7 @@
 #5/17/2022
 #This code will find the words that are present in both strings
 def words_in_both(sentence,sentence_2):
+    """This will return the common words that are in both sentences"""
     sentence = sentence.lower()
     sentence_2 = sentence_2.lower()
     words = sentence.split()
